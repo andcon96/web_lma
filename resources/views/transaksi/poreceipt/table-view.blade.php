dd74c1db5f210c9cb1eed115483a262df7c9e4b9
tommywcak <tomy0344@gmail.com>
2022-04-28 09:44:19 +0700

    update  po receipt part 4

--- resources/views/transaksi/poreceipt/table-view.blade.php
+++ resources/views/transaksi/poreceipt/table-view.blade.php
@@ -1,4 +1,4 @@
-<div class="table-responsive col-lg-12 col-md-12 mb-4 tag-container" style="overflow-x: auto; display: block;white-space: nowrap;">
+<div class="table-responsive col-lg-12 col-md-12 mb-4 tag-container" style="overflow-x: auto; overflow-y: hidden; display: block;white-space: nowrap;">
   <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
     <thead>
       <tr>
