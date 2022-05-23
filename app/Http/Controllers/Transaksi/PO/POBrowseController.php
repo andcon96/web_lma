114f11f08425b3a112da5b8f198f88d753910dc1
tommywcak <tomy0344@gmail.com>
2022-05-23 15:42:44 +0700

    tambah order by di po receipt browse

--- app/Http/Controllers/Transaksi/PO/POBrowseController.php
+++ app/Http/Controllers/Transaksi/PO/POBrowseController.php
@@ -30,7 +30,7 @@ class POBrowseController extends Controller
         }
 
 
-        $datas = $datas->whereRelation('getUser.getRoleType','usertype', 'office')->paginate(10);
+        $datas = $datas->whereRelation('getUser.getRoleType','usertype', 'office')->orderBy('id','desc')->paginate(10);
 
         return view('transaksi.porcpbrowse.index',compact('datas','supps'));
     }
