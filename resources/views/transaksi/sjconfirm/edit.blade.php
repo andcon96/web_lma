4503aeac5cafbcad7bf0805158049ca534debd1d
tommywcak <tomy0344@gmail.com>
2022-06-08 16:14:58 +0700

    update tambah potong dp

--- resources/views/transaksi/sjconfirm/edit.blade.php
+++ resources/views/transaksi/sjconfirm/edit.blade.php
@@ -66,7 +66,7 @@
         <div class="row mb-3">
             <label for="potongdp" class="col-form-label col-md-3" style="margin-left:25px">{{ __('Potong DP') }}</label>
             <div class="col-md-3">
-                <input id="potongdp" type="number" class="form-control" name="potongdp" min="0" step="0.01" value="{{ old('potongdp') ? old('potongdp') : 0" maxlength="24" autocomplete="off">
+                <input id="potongdp" type="number" class="form-control" name="potongdp" min="0" step="0.01" value="{{ old('potongdp') ? old('potongdp') : 0 }}" maxlength="24" autocomplete="off">
             </div>
         </div>
         <div class="form-group row col-md-12">
