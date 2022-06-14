b467fbea8ee9f3bc28fba999a085bdbab61ff0f6
andcon96 <78584931+andcon96@users.noreply.github.com>
2022-06-14 17:41:33 +0700

    tst

--- app/Services/WSAServices.php
+++ app/Services/WSAServices.php
@@ -1182,7 +1182,7 @@ class WSAServices
             curl_close($curl);
         }
 
-        dd($qdocResponse);
+        dd($qdocResponse,$qdocRequest);
         if(is_bool($qdocResponse)){
             return false;
         }
