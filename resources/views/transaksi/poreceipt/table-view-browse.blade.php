b0558b83261a1a2855f6f93938dcaa82350f9550
tommywcak <tomy0344@gmail.com>
2022-06-16 15:58:46 +0700

    tampilan table detail di dalam table utama

--- resources/views/transaksi/poreceipt/table-view-browse.blade.php
+++ resources/views/transaksi/poreceipt/table-view-browse.blade.php
@@ -32,9 +32,9 @@
             @forelse ( $podetail as $showdetail )
               @if ( $showdetail->po_nbr == $show->po_nbr )
                 <tr>
-                  <td style="width: 5%">{{ $showdetail->pod_line }} adadaad</td>
-                  <td style="width: 10%">{{ $showdetail->pod_part }} -- {{ $showdetail->pod_partdesc }}</td>
-                  <td style="width: 10%">{{ number_format($showdetail->pod_qty_ord - $showdetail->pod_qty_rcvd,2) }}</td>
+                  <td style="width: 5%;">{{ $showdetail->pod_line }}</td>
+                  <td style="width: 10%;">{{ $showdetail->pod_part }} -- {{ $showdetail->pod_partdesc }}</td>
+                  <td style="width: 10%;">{{ number_format($showdetail->pod_qty_ord - $showdetail->pod_qty_rcvd,2) }}</td>
                 </tr>
               @endif
             @empty
