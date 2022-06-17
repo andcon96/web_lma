505f2b488ca7bffa25c265c3c408c1e8f19493ec
tommywcak <tomy0344@gmail.com>
2022-06-17 15:24:13 +0700

    blade create surat jalan

--- resources/views/transaksi/suratjalan/create.blade.php
+++ resources/views/transaksi/suratjalan/create.blade.php
@@ -62,13 +62,13 @@
     </div>
 
 
-    <div class="d-flex row text-right">
+    <div class="row">
         <div class="col-lg-2 col-md-2 col-sm-12 py-2">
             <input type="submit" name="submit" id='s_btnconf' value='Submit' class="btn bt-action" style="width: 100% !important;">
         </div>
         <div class="col-lg-2 col-md-2 col-sm-12 py-2">
             <a href="{{route('searchSO',['sjnbr'=>$so[0]->so_cust])}}" class="btn bt-action" style="width: 100% !important;">Cancel</a>
-            <button type="button" class="btn btn-info float-right mt-3" id="s_btnloading" style="display:none;">
+            <button type="button" class="btn btn-info" id="s_btnloading" style="display:none;">
                 <i class="fa fa-circle-o-notch fa-spin"></i> &nbsp;Loading
             </button>
         </div>
