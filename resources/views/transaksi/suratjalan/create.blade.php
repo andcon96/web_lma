2c3be950c5110bcdccc7b253bc1cd8f712928f72
tommywcak <tomy0344@gmail.com>
2022-06-17 14:46:40 +0700

    blade create surat jalan

--- resources/views/transaksi/suratjalan/create.blade.php
+++ resources/views/transaksi/suratjalan/create.blade.php
@@ -61,18 +61,20 @@
         </div>
     </div>
 
-
-    <div class="row">
-        <div class="col-lg-2 col-md-2 col-sm-12 py-2 float-right">
-            <input type="submit" name="submit" id='s_btnconf' value='Submit' class="btn bt-action" style="width: 100% !important;">
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-12 py-2 float-right">
-            <a href="{{route('searchSO',['sjnbr'=>$so[0]->so_cust])}}" class="btn bt-action" style="width: 100% !important;">Cancel</a>
-            <button type="button" class="btn btn-info float-right mt-3" id="s_btnloading" style="display:none;">
-                <i class="fa fa-circle-o-notch fa-spin"></i> &nbsp;Loading
-            </button>
+    <div class="d-flex">
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-12 py-2 float-right">
+                <input type="submit" name="submit" id='s_btnconf' value='Submit' class="btn bt-action" style="width: 100% !important;">
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-12 py-2 float-right">
+                <a href="{{route('searchSO',['sjnbr'=>$so[0]->so_cust])}}" class="btn bt-action" style="width: 100% !important;">Cancel</a>
+                <button type="button" class="btn btn-info float-right mt-3" id="s_btnloading" style="display:none;">
+                    <i class="fa fa-circle-o-notch fa-spin"></i> &nbsp;Loading
+                </button>
+            </div>
         </div>
     </div>
+    
 
 </form>
 
