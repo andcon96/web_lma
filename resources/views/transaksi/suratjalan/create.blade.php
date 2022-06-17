5387d5a91893b9dcae99304f093a71ae413a3964
tommywcak <tomy0344@gmail.com>
2022-06-17 13:57:34 +0700

    table create

--- resources/views/transaksi/suratjalan/create.blade.php
+++ resources/views/transaksi/suratjalan/create.blade.php
@@ -62,7 +62,7 @@
     </div>
 
     <div class="form-group row offset-lg-1 col-lg-12 col-col-md-12 col-sm-12 col-xs-12">
-        <div class="float-right col-lg-12">
+        <div class="float-right row col-lg-12">
             <input type="submit" name="submit" id='s_btnconf' value='Submit' class="btn bt-action float-right mt-3">
             <a href="{{route('searchSO',['sjnbr'=>$so[0]->so_cust])}}" class="btn bt-action float-right mt-3 mr-3">Cancel</a>
             <button type="button" class="btn btn-info float-right mt-3" id="s_btnloading" style="display:none;">
