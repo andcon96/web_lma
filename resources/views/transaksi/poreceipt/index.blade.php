9f9fe8352829a5e4cd06abacaf029d3a90efaa2d
tommywcak <tomy0344@gmail.com>
2022-06-20 12:10:48 +0700

    push

--- resources/views/transaksi/poreceipt/index.blade.php
+++ resources/views/transaksi/poreceipt/index.blade.php
@@ -13,7 +13,7 @@
 <form action="{{route('searchPO')}}" method="GET">
     <div class="form-group row">
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-            <label for="sjnbr" class="col-form-label text-md-right" style="margin-left:25px">{{ __('PO No.') }}</label>
+            <label for="sjnbr" class="col-form-label text-md-right" style="margin-left:25px">PO No.</label>
         </div>
         <div class="col-xl-3 col-lg-3 col-md-8 col-sm-12 col-xs-12">
             <input id="sjnbr" type="text" class="form-control" name="sjnbr" required>
@@ -21,7 +21,7 @@
     </div>
     <div class="form-group row">
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-            <label for="pokontrak" class="col-form-label text-md-right" style="margin-left:25px">{{ __('PO Kontrak') }}</label>
+            <label for="pokontrak" class="col-form-label text-md-right" style="margin-left:25px">PO Kontrak</label>
         </div>
         <div class="col-xl-3 col-lg-3 col-md-8 col-sm-12 col-xs-12">
             <input id="pokontrak" type="text" class="form-control" name="pokontrak" required>
@@ -29,7 +29,7 @@
     </div>
     <div class="form-group row">
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-            <label for="suppcode" class="col-form-label text-md-right" style="margin-left:25px">{{ __('Supplier Name') }}</label>
+            <label for="suppcode" class="col-form-label text-md-right" style="margin-left:25px">Supplier Name</label>
         </div>
         <div class="col-xl-4 col-lg-4 col-md-9 col-sm-12 col-xs-12">
             <select name="suppcode" id="suppcode" class="form-control" required>
