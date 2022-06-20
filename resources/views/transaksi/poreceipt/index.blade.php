7f024a834dcbcee350327892bb590a343140c393
tommywcak <tomy0344@gmail.com>
2022-06-20 09:47:11 +0700

    perubahan search poreceipt

--- resources/views/transaksi/poreceipt/index.blade.php
+++ resources/views/transaksi/poreceipt/index.blade.php
@@ -15,18 +15,15 @@
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
             <label for="sjnbr" class="col-form-label text-md-right" style="margin-left:25px">{{ __('PO No.') }}</label>
         </div>
-        <div class="col-xl-2 col-lg-2 col-md-8 col-sm-12 col-xs-12">
+        <div class="col-xl-3 col-lg-3 col-md-8 col-sm-12 col-xs-12">
             <input id="sjnbr" type="text" class="form-control" name="sjnbr" required>
-        </div>
-        <div class="offset-md-3 offset-lg-0 offset-xl-0 offset-sm-0 offset-xs-0" id='btn'>
-            <input type="submit" class="btn bt-ref" id="btnsearch" value="Search" style="margin-left:15px;" />
         </div>
     </div>
     <div class="form-group row">
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
             <label for="pokontrak" class="col-form-label text-md-right" style="margin-left:25px">{{ __('PO Kontrak') }}</label>
         </div>
-        <div class="col-xl-2 col-lg-2 col-md-8 col-sm-12 col-xs-12">
+        <div class="col-xl-3 col-lg-3 col-md-8 col-sm-12 col-xs-12">
             <input id="pokontrak" type="text" class="form-control" name="pokontrak" required>
         </div>
     </div>
@@ -34,13 +31,18 @@
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
             <label for="suppcode" class="col-form-label text-md-right" style="margin-left:25px">{{ __('Supplier Name') }}</label>
         </div>
-        <div class="col-xl-4 col-lg-4 col-md-10 col-sm-12 col-xs-12">
+        <div class="col-xl-4 col-lg-4 col-md-9 col-sm-12 col-xs-12">
             <select name="suppcode" id="suppcode" class="form-control" required>
                 <option value="">Select Data</option>
                 @foreach ($suppdat as $supps )
                 <option value="{{$supps->supp_code}}">{{$supps->supp_code}} - {{$supps->supp_name}}</option>
                 @endforeach
             </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12" id='btn'>
+            <input type="submit" class="btn bt-ref" id="btnsearch" value="Search" style="margin-left:15px;" />
         </div>
     </div>
 </form>
