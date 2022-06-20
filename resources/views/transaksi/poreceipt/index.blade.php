255d5281c2704c379107bab51bbcf223d27e71b7
tommywcak <tomy0344@gmail.com>
2022-06-20 09:53:23 +0700

    perubahan search poreceipt

--- resources/views/transaksi/poreceipt/index.blade.php
+++ resources/views/transaksi/poreceipt/index.blade.php
@@ -41,8 +41,8 @@
         </div>
     </div>
     <div class="form-group row">
-        <div class="col-xl-7 col-lg-7 col-md-3 col-sm-12 col-xs-12">
-            <input type="submit" class="btn bt-ref col-md-12" id="btnsearch" value="Search" />
+        <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12">
+            <input type="submit" class="btn bt-ref offset-md-1 col-md-3" id="btnsearch" value="Search" />
         </div>
     </div>
 </form>
