7cd008f0bc93b8652817f80f71b996478e7b4fb4
tommywcak <tomy0344@gmail.com>
2022-06-22 10:31:35 +0700

    update index POBrowseController

--- app/Http/Controllers/Transaksi/PO/POBrowseController.php
+++ app/Http/Controllers/Transaksi/PO/POBrowseController.php
@@ -25,16 +25,19 @@ class POBrowseController extends Controller
         $supps = POhist::groupBy('ph_supp')->select('ph_supp','ph_suppname')->get();
 
         if($req->ponbr){
+            dump($req->ponbr);
             $datas->where('ph_ponbr',$req->ponbr);
         }
         if($req->supp){
+            dump($req->supp);
             $datas->where('ph_supp',$req->supp);
         }
         if($req->receiptdate){
-            dd($req->receiptdate);
+            dump($req->receiptdate);
             $datas->where('ph_receiptdate',$req->receiptdate);
         }
         if($req->pocon){
+            dump($req->pocon);
             $datas->where('ph_pokontrak',$req->pocon);
         }
 
