0b81831160f15ce413db2897b5d6372723ac857f
tommywcak <tomy0344@gmail.com>
2022-06-23 15:42:32 +0700

    perbaikan blade untuk format export excel pohist

--- app/Exports/POhistExport.php
+++ app/Exports/POhistExport.php
@@ -6,8 +6,9 @@ use App\Models\Export\ExportPO;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
 
-class POhistExport implements FromView, ShouldAutoSize
+class POhistExport implements FromView, ShouldAutoSize, WithStrictNullComparison
 {
     public function view():View
     {
