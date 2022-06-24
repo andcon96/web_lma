23ba6e503606644c01f72be72eaee25f3e965174
tommywcak <tomy0344@gmail.com>
2022-06-24 17:18:11 +0700

    perbaikan tampilan mobile create sj

--- resources/views/transaksi/suratjalan/index.blade.php
+++ resources/views/transaksi/suratjalan/index.blade.php
@@ -12,16 +12,16 @@
 
 <form action="{{route('searchSO')}}" method="GET">
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
             <label for="sonbr" class="col-form-label text-md-right">{{ __('SO No.') }}</label>
         </div>
-        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-12 col-xs-12">
             <input type="text" id="sonbr" name="sonbr" class="form-control" value="" required /> 
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
             <label for="sjnbr" class="col-form-label text-md-right ml-1 ml-sm-0 ml-xs-0">{{ __('Customer') }}</label>
         </div>
-        <div class="col-xl-4 col-lg-4 col-md-8 col-sm-12 col-xs-12">
+        <div class="col-xl-2 col-lg-2 col-md-8 col-sm-12 col-xs-12">
             <select id="sjnbr" class="form-control" name="sjnbr" required>
                     <option value="">Select Data</option>
                 @foreach ( $custdat as $show )
