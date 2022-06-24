662a5d6a82a2cc159a7c41781ed98f0b1cc7aeff
tommywcak <tomy0344@gmail.com>
2022-06-24 10:18:22 +0700

    perbaikan SJExport

--- app/Exports/SJExport.php
+++ app/Exports/SJExport.php
@@ -8,7 +8,7 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class SJExport implements FromView, ShouldAutoSize,
+class SJExport implements FromView, ShouldAutoSize
 {
     public function view(): View
     {
