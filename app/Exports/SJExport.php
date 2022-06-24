cc235b039427f5b6d06a7dc26da852816915a387
tommywcak <tomy0344@gmail.com>
2022-06-24 10:17:07 +0700

    perbaikan SJExport

--- app/Exports/SJExport.php
+++ app/Exports/SJExport.php
@@ -6,8 +6,9 @@ use App\Models\Transaksi\SuratJalan;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class SJExport implements FromView
+class SJExport implements FromView, ShouldAutoSize,
 {
     public function view(): View
     {
