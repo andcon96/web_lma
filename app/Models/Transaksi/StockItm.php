11eb9710e7d646f3b2356081b14630dcab240571
tommywcak <tomy0344@gmail.com>
2022-06-27 10:21:31 +0700

    perbaikan model stock item

--- app/Models/Transaksi/StockItm.php
+++ app/Models/Transaksi/StockItm.php
@@ -10,4 +10,13 @@ class StockItm extends Model
     use HasFactory;
 
     public $table = 'stockitm';
+
+    protected $fillable = [
+        'itemdom',
+        'item_loc',
+        'item_nbr',
+        'item_desc',
+        'item_um',
+        'item_qtyoh',
+    ];
 }
