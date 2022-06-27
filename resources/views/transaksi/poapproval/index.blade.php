8904a78e44a7fdc256c5342de5c0c1a64fdf5c6e
tommywcak <tomy0344@gmail.com>
2022-06-27 15:04:21 +0700

    po approval mobile view

--- resources/views/transaksi/poapproval/index.blade.php
+++ resources/views/transaksi/poapproval/index.blade.php
@@ -13,7 +13,7 @@
 <form id="searchinvc" action="{{route('searchpoinvc')}}" method="GET">
     <div class="form-group row">
         <label for="ponbr" class="col-form-label text-md-right">{{ __('Invoice Reference') }}</label>
-        <div class="col-xl-2 col-lg-2 col-md-8 col-sm-12 col-xs-12">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
             <input id="ponbr" type="text" class="form-control" name="ponbr">
         </div>
         <!-- <label for="receiptdate" class="col-form-label text-md-right" style="margin-left:25px">{{ __('Receipt Date') }}</label>
@@ -22,7 +22,7 @@
                 value="{{ Carbon\Carbon::parse(now())->format('d-m-Y')  }}" readonly>
         </div> -->
 
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-12 col-xs-12">
             <input type="submit" class="btn bt-ref" id="btnsearch" value="Search" style="width: 100%;" />
             <button type="button" class="btn btn-info" id="s_btnloading" style="display:none;width: 100%;">
                 <i class="fa fa-circle-o-notch fa-spin"></i> &nbsp;Loading
