b24ac2ba165c20fd09bf8ca435a25294ee13c2f4
tommywcak <tomy0344@gmail.com>
2022-06-27 15:28:00 +0700

    poreceipt browse

--- resources/views/transaksi/porcpbrowse/index.blade.php
+++ resources/views/transaksi/porcpbrowse/index.blade.php
@@ -42,7 +42,7 @@
         <div class="col-xl-3 col-lg-3 col-md-4 col-sm-12 col-xs-12">
             <input id="receiptdate" type="text" class="form-control" name="receiptdate" autocomplete="off" value="{{request()->input('receiptdate') ? request()->input('receiptdate') : '' }}">
         </div>
-        <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-xs-12 mt-xl-0 mt-lg-0 mt-3">
             <button class="btn bt-ref" id="btnsearch" value="search">Search</button>
             <button class="btn bt-action ml-2" id='btnrefresh' style="width: 40px !important"><i class="fa fa-sync"></i></button>
         </div>
