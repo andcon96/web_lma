5ce31b6f43cbf2d3e75c3ef584407f8d153d6860
tommywcak <tomy0344@gmail.com>
2022-06-29 14:23:06 +0700

    tambah kolom status di table rcpt_unplanned

---
+++ database/migrations/2022_06_29_142140_add_status_to_rcpt_unplanned.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToRcptUnplanned extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rcpt_unplanned', function (Blueprint $table) {
+            //
+            $table->enum('status',['Open','Closed'])->default('New');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rcpt_unplanned', function (Blueprint $table) {
+            //
+            $table->dropColumn('status');
+        });
+    }
+}
