8fa23d05d8b5bd45713ce6bf584d71fba37df1c8
tommywcak <tomy0344@gmail.com>
2022-07-04 11:39:23 +0700

    dd

--- app/Http/Controllers/Transaksi/PO/POReceiptController.php
+++ app/Http/Controllers/Transaksi/PO/POReceiptController.php
@@ -52,6 +52,8 @@ class POReceiptController extends Controller
             $kontraktampung = $req->pokontrak;
         }
 
+        dd($ponbrtampung,$supptampung,$kontraktampung);
+
         // WSA QAD
         $po_receipt = (new WSAServices())->wsagetpo($ponbrtampung,$supptampung,$kontraktampung);
 
