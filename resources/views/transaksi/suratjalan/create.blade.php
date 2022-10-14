54a7f1c6c66da2987076d5dc998f3de7b802f2d3
tommywcak <tomy0344@gmail.com>
2022-10-14 11:25:27 +0700

    cek

--- resources/views/transaksi/suratjalan/create.blade.php
+++ resources/views/transaksi/suratjalan/create.blade.php
@@ -11,7 +11,7 @@
 @section('content')
 
 
-<form method="get" action="{{route('suratjalan.store')}}" id='submit'>
+<form method="post" action="{{route('suratjalan.store')}}" id='submit'>
     @method('POST')
     @csrf
 
@@ -46,7 +46,7 @@
         </div>
         <label for="nopol" class="col-md-2 col-form-label text-md-right">{{ __('No Polis') }}</label>
         <div class="col-xl-3 col-lg-3 col-md-8 col-sm-12 col-xs-12">
-            <input id="nopol" type="text" class="form-control" name="nopol" value="{{request()->input('nopol')}}" required>
+            <input id="nopol" type="text" class="form-control" name="nopol" value="{{ old('nopol') }}" required>
         </div>
     </div>
 
